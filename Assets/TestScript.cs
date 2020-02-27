a3b52369410c7a18f4aefaa0c10fcf8aa7902592
jacraade <jac@raade.at>
2020-02-27 13:20:48 +0100

    added project

---
+++ Assets/TestScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class TestScript : MonoBehaviour {
+    public void Test(CallbackContext context) {
+        Debug.Log(context.phase);
+    }
+}
